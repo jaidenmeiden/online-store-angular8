fe581c7c9c752e1e5419c32a0e2cd171a7a64777
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-22 15:56:21 -0500

    Se implementan los componentes en el home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  mySwiper: Swiper;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.mySwiper = new Swiper('.swiper-container');
   }
 
 }
