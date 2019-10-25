9a39e29d70d5122c29ed5c33451b113967e4fa33
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-25 17:07:17 -0500

    Implementación del Lazy Loading (Implica configurar el modulo 'home'). Se implementa una estrategia de precarga.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
 
 import {LayoutComponent} from "./components/layout/layout.component";
 import {ProductsComponent} from './components/products/products.component';
@@ -47,7 +47,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
