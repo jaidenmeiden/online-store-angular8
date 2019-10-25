3541d1d6735c91044028c51c80f68ea3180c7442
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-25 16:53:20 -0500

    Implementación del Lazy Loading (Implica configurar el modulo 'home'). Se importa el componente 'home' dentro del modulo 'home'.

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BannerComponent } from './components/banner/banner.component';
-import { HomeComponent } from './home.component';
+import { HomeComponent } from './components/home/home.component';
 
 import { HomeRoutingModule } from './home-routing.module';
 
