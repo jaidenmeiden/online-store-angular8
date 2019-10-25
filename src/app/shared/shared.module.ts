de68981cb2983866daa2a0c36b24497f855fb3ca
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-25 18:09:59 -0500

    Creando un shared module. Se importan del 'shared.module.ts' los pipes y las directivas que se van a utilizar en el modulo 'shared'.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ExponentialPipe } from './pipes/exponential.pipe';
+
+import { HighlightDirective } from './directives/highlight.directive';
+
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
@@ -8,6 +12,8 @@ import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
+    ExponentialPipe,
+    HighlightDirective,
     HeaderComponent,
     FooterComponent
   ],
