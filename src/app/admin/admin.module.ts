8fec6c277d038bec0a612411e96c693c2c709e8e
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-26 10:12:57 -0500

    Se crea el 'table' y 'dashboard' con 'ng generate @angular/material:table admin/components/table' y 'ng generate @angular/material:dashboard admin/components/dashboard'. Automáticamente se agrega en AdminModule todos los modulos de Material que necesitamos.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -7,18 +7,38 @@ import { MaterialModule } from '../material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NavComponent } from './components/nav/nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
+import { TableComponent } from './components/table/table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     ProductFormComponent,
-    NavComponent
+    NavComponent,
+    TableComponent,
+    DashboardComponent
   ],
   imports: [
     CommonModule,
     AdminRoutingModule,
     MaterialModule,
     ReactiveFormsModule,
-    LayoutModule
+    LayoutModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class AdminModule { }
