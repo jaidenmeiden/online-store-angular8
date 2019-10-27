20e9f19d13cb15f4576521098ea72f8c03642fe4
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-27 09:15:01 -0500

    Lista de inventarios y detalle con Material

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ExponentialPipe } from './pipes/exponential.pipe';
-
 import { HighlightDirective } from './directives/highlight.directive';
 
 import { HeaderComponent } from './components/header/header.component';
@@ -30,7 +30,8 @@ import { MaterialModule } from '../material/material.module';
   imports: [
     CommonModule,
     RouterModule,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class SharedModule { }
