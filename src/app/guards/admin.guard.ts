31ffe3c63c73738f38e8e6bc3d89c6ebde27f36c
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-27 22:59:23 -0500

    Se hacen ajustes adicionales y correcciones en el Guardian

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 
@@ -10,7 +10,8 @@ import {AuthService} from '../core/services/auth.service';
 })
 export class AdminGuard implements CanActivate {
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
   ) {
 
   }
@@ -19,8 +20,13 @@ export class AdminGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.authService.hasUser().pipe(
-      tap(user => console.log(user)), // Intercepta el flujo de datos
-      map(user => user === null ? false : true)
+      map(user => user === null ? false : true),
+      tap(hasUser => {
+        console.log(hasUser);
+        if (!hasUser) {
+          this.router.navigate(['/auth/login']);
+        }
+      }),
     );
   }
 
