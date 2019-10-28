2585b8bceaec0746c3b3b1e9b4290128c814f26d
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-27 21:58:25 -0500

    Se instala y configura Angular @angular/fire con 'npm install firebase @angular/fire --save'.

--- src/environments/environment.stag.ts
+++ src/environments/environment.stag.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
-  url_api: 'https://platzi-store-stag.herokuapp.com'
+  url_api: 'https://platzi-store-stag.herokuapp.com',
+  firebase: {
+    apiKey: 'AIzaSyARcsHHd-sfRKtFcdwfg7fKCgG3SEc7U7A',
+    authDomain: 'platzistore-1a4d8.firebaseapp.com',
+    databaseURL: 'https://platzistore-1a4d8.firebaseio.com',
+    projectId: 'platzistore-1a4d8',
+    storageBucket: 'platzistore-1a4d8.appspot.com',
+    messagingSenderId: '53056686818'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,17 @@
 
 export const environment = {
   production: false,
-  url_api: 'https://platzi-store.herokuapp.com'
+  url_api: 'https://platzi-store.herokuapp.com',
+  firebase: {
+    apiKey: 'AIzaSyARcsHHd-sfRKtFcdwfg7fKCgG3SEc7U7A',
+    authDomain: 'platzistore-1a4d8.firebaseapp.com',
+    databaseURL: 'https://platzistore-1a4d8.firebaseio.com',
+    projectId: 'platzistore-1a4d8',
+    storageBucket: 'platzistore-1a4d8.appspot.com',
+    messagingSenderId: '53056686818'
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
