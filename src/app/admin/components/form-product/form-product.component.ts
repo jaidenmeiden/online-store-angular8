b36ef85c450c1403944be14c49c79a6cc11da99d
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-27 23:29:55 -0500

    Subiendo una imagen a Firebase Storage

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import {AngularFireStorage} from '@angular/fire/storage';
+import {Observable} from 'rxjs';
+import {finalize} from 'rxjs/operators';
 
 import {ProductsService} from '../../../core/services/products.service';
 import {MyValidators} from '../../../utils/my-validators';
@@ -13,11 +16,13 @@ import {MyValidators} from '../../../utils/my-validators';
 export class FormProductComponent implements OnInit {
 
   form: FormGroup;
+  image$: Observable<any>;
 
   constructor(
     private formBuilder: FormBuilder,
     private productsService: ProductsService,
-    private router: Router
+    private router: Router,
+    private storage: AngularFireStorage
   ) {
     this.buildForm();
   }
@@ -26,7 +31,7 @@ export class FormProductComponent implements OnInit {
   }
 
   saveProduct(event: Event) {
-    event.preventDefault(); // Quita la opción por defecto de recargar la pagina
+    event.preventDefault();
     if (this.form.valid) {
       const product = this.form.value;
       this.productsService.createProduct(product)
@@ -35,6 +40,25 @@ export class FormProductComponent implements OnInit {
           this.router.navigate(['./admin/products']);
         });
     }
+  }
+
+  uploadFile(event) {
+    const file = event.target.files[0];
+    const path = 'image.png';
+    const fileRef = this.storage.ref(path);
+    const task = this.storage.upload(path, file);
+
+    task.snapshotChanges()
+      .pipe(
+        finalize(() => {
+          this.image$ = fileRef.getDownloadURL();
+          this.image$.subscribe(url => {
+            console.log(url);
+            this.form.get('image').setValue(url);
+          });
+        })
+      )
+      .subscribe();
   }
 
   private buildForm() {
