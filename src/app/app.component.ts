daadb3a6e15220cb73960fa064d33fd77c8d5267
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-31 22:02:09 -0500

    Google Analytics y Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+import { filter } from 'rxjs/operators';
+
+declare var gtag;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  constructor(
+    private router: Router
+  ) {
+    const navEndEvents$ = this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd)
+      );
+
+    navEndEvents$.subscribe((event: NavigationEnd) => {
+      gtag('config', 'UA-149890792-1', {
+        page_path: event.urlAfterRedirects
+      });
+    });
+  }
 }
