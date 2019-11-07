3324df5e816204f1214b16039affaa57f1651567
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-11-07 13:44:09 -0500

    Implementando Angular Universal (https://angular.io/guide/universal)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     LayoutComponent,
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     FormsModule,
     SharedModule,
