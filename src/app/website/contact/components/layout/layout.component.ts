92a8c96454b28b97137012ef05fbad10260118a5
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-11-07 20:08:33 -0500

    Pipe Async (Automaticamente destruye un observable) - Tema Change Detection

--- src/app/website/contact/components/layout/layout.component.ts
+++ src/app/website/contact/components/layout/layout.component.ts
@@ -1,8 +1,10 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { GeneratorService } from '../../../../core/services/generator.service';
 import { EmployeeData } from '../../../../core/models/employee.model';
-import {Subscription} from "rxjs";
+import { Observable, Subscription } from 'rxjs';
+
+import { tap } from 'rxjs/operators';
 
 const names = ['Jaiden', 'Diana', 'Alfredo', 'Marcela', 'Daniel', 'Camila'];
 
@@ -17,22 +19,28 @@ export class LayoutComponent implements OnInit, OnDestroy {
   bList: EmployeeData[] = [];
 
   value: number;
+  value$: Observable<number>;
 
   sub$: Subscription;
 
   constructor(
     private generatorService: GeneratorService
-  ) { }
+  ) {
+    this.value$ = this.generatorService.getData()
+      .pipe(
+        tap(num => console.log('value Async: ' + num))
+      );
+  }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.salesList = this.generatorService.generate(names, [10, 20], 10);
     this.bList = this.generatorService.generate(names, [10, 20], 10);
 
     this.sub$ = this.generatorService.getData()
       .subscribe(value => {
         this.value = value;
-        console.log(this.value);
-      })
+        console.log('value Subscription: ' + this.value);
+      });
   }
 
   ngOnDestroy(): void {
